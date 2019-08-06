bda84bfab5e0f9470d3b3dbb3db30f89468af426
Sharon Watkins <swatkins876@gmail.com>
2019-08-06 15:18:10 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch =>{},
+      :matiarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :montague => {
+      :patriarch =>{},
+      :matiarch => {},
+      :heroine => {},
+      :heroine_friends => []
       }}
 
   
